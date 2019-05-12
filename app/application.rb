55b4a48370be1142fd7407bbecda13e4ed7170cc
MarcoMiranda05 <marmirand@hotmail.com>
2019-05-12 20:58:01 +0100

    Done.

---
+++ app/application.rb
@@ -0,0 +1,16 @@
+class Application
+
+    def call(env)
+        resp = Rack::Response.new
+
+        day_time = Time.now.hour
+
+        if day_time >= 12 
+            resp.write "Good Afternoon!"
+        else
+            resp.write "Good Morning!"
+        end
+
+        resp.finish
+    end
+end
